adcb11887972fce6ca8afc2cbaf9fff2ca459510
JDrewniak2 <saylen223@wp.pl>
2022-05-31 20:22:17 +0200

    Do 8 zadania

--- src/pl/lublin/wsei/java/cwiczenia/Main.java
+++ src/pl/lublin/wsei/java/cwiczenia/Main.java
@@ -1,15 +1,20 @@
 package pl.lublin.wsei.java.cwiczenia;
 
 public class Main {
-    /**
-     *
-     *  @param args
-     */
+
     public static void main(String[] args) {
-	// Komentarz pierwszy
-        /*
-        Komentarz wielolinijkowy drugi
-         */
-        System.out.println("Hello Java world\n");
+
+        int a = 3;
+        double c;
+        double b = 4.21;
+        String s = "jakiś tekst";
+
+        System.out.printf("a = %d, b = %.2f, s = %20s %n",a,b,s);
+        System.out.printf("Nazwaliśmy to "+"witaminą B3");
+        System.out.printf("alfa/tsin(alfa)\n");
+        for (int i = 0; i < 370; i+=10)
+            System.out.printf("%d\t%f\t\n", i, Math.sin(i/360.0*2*Math.PI));
+        for (int j = 1; j < 100; j = j * 2)
+            System.out.printf("%d\t%f\t%s\n", j, c = Math.log(j) / Math.log(2), c + j );
     }
 }
